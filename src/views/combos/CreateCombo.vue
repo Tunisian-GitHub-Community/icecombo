0a359ce28f2badfe51b44e8833d3df3c69429b9a
ayoub3bidi <mrayoubabidi@gmail.com>
2021-07-27 15:28:23 +0100

    🚧 Adding uploadImages function

--- src/views/combos/CreateCombo.vue
+++ src/views/combos/CreateCombo.vue
@@ -31,16 +31,22 @@
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import {ref} from 'vue'
+import useStorage from '@/composables/useStorage'
 
 export default {
     setup() {
+        const { filePath, url, uploadImage } = useStorage()
+
         const title = ref('')
         const description = ref('')
         const file = ref(null)
         const fileError = ref(null)
 
-        const handleSubmbit = () => {
+        const handleSubmit = async () => {
+            if (file.value) {
+                await uploadImage(file.value)
+            }
         }
 
         // allowed file types
@@ -49,6 +55,7 @@ export default {
         const handleChange = (e) => {
             const selected = e.target.files[0]
             console.log(selected)
+
             if (selected && types.includes(selected.type)) {
                 file.value = selected
                 fileError.value = null
@@ -58,7 +65,7 @@ export default {
             }
         }
 
-        return { title, description, handleSubmbit, handleChange, fileError }
+        return { title, description, handleSubmit, handleChange, fileError }
     }
 }
 </script>
